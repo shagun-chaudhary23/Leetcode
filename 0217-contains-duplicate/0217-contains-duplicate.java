c9f64faa370c478592b129602b777b51fb98118d
Shagun Chaudhary <shagunchaudhary0723@gmail.com>
2026-08-19 22:58:06 +0530

    Time: 24 ms (28.52%), Space: 81.2 MB (96%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int len=nums.length;
+        Arrays.sort(nums);
+        for(int i=0;i<len-1;i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
