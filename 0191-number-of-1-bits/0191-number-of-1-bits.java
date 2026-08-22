704e19443e279504d11f81ae9c173dc2b650dbaa
Shagun Chaudhary <shagunchaudhary0723@gmail.com>
2026-08-23 00:00:40 +0530

    Time: 0 ms (100%), Space: 42.2 MB (91.93%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,19 +1,17 @@
+import java.util.*;
 class Solution {
     public int hammingWeight(int n) {
-        int l = 0;
-        // Find the highest power of 2 using regular int
-        while ((1 << l) <= n && (1 << l) > 0) {
+        int l=0;
+        while((1L << l)<=n){
             l++;
         }
-        l--; 
-
-        int count = 0;
-        int num = 0; // Regular int
-        
-        while (l >= 0) {
-            int val = 1 << l; // No type mismatch anymore
-            if (num + val <= n) {
-                num += val;
+        l--;
+        int count =0;
+        int num=0;
+        while(l>=0){
+            int val=1 << l;
+            if(num+val<=n){
+                num=num+val;
                 count++;
             }
             l--;
